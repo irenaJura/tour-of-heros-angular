cd6ec6acdec43b7f78f08a03dc228b54884f6087
Irena Jurasek <irenaskydive@gmail.com>
2021-06-06 16:50:38 +0200

    use *ngIf for conditional html block

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,16 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  }
-
   heroes = HEROES;
-  
+  selectedHero?: Hero;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
